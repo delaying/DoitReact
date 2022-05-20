a5c4da718855cd75b812abaa80298b20a33e554c
delaying <pgy5638@naver.com>
2022-05-20 21:06:03 +0900

    prttier setting

---
+++ src/01/RCC.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class RCC extends Component {
+    render() {
+        let text =''
+        return <div name='name'>{text}</div>;
+         
+        }
+    }
+}
+
+export default RCC;
