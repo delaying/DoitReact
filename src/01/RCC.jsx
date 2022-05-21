c5451643a63a22472111e5e60d85724eddf25de0
delaying <pgy5638@naver.com>
2022-05-21 19:44:36 +0900

    using class

--- src/01/RCC.jsx
+++ src/01/RCC.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 
+class Shape {
+  static create(x, y) {
+    return new Shape(x, y);
+  }
+  name = 'Shape';
+  constructor(x, y) {
+    this.move(x, y);
+  }
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  area() {
+    return 0;
+  }
+}
+let s = new Shape(0, 0);
+s.area(); // 0
+
+class Circle extends Shape {
+  constructor(x, y, radius) {
+    super(x, y);
+    this.radius = radius;
+  }
+  area() {
+    if (this.radius === 0) return super.area();
+    return this.radius * this.radius;
+  }
+}
+let c = new Circle(0, 0, 10);
+c.area(); //100
 
 class RCC extends Component {
-    render() {
-        let text =''
-        return <div name='name'>{text}</div>;
-         
-        }
-    }
+  render() {
+    let text = '';
+
+    return <div name="name">{text}</div>;
+  }
 }
 
 export default RCC;
