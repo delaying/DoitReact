711fc2a60c18a26e646ef9d84c78e45211755c27
delaying <pgy5638@naver.com>
2022-05-25 16:51:38 +0900

    jsx dom event property

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import TodayPlan from './03/TodayPlan';
+import Counter3 from './03/Counter3';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <TodayPlan />
+        <Counter3 />
       </div>
     );
   }
