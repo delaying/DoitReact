043507667be686aca25d2c4b9d81a219fd1e437f
delaying <pgy5638@naver.com>
2022-05-25 21:29:59 +0900

    하이어오더, 커링

---
+++ .storybook/addons.js
@@ -0,0 +1,2 @@
+//확장 도구는 이곳에 추가함.
+import '@storybook/addon-actions/register';
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import TodayPlan from './03/TodayPlan';
 import Counter3 from './03/Counter3';
+import Input from './03/Input';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Counter3 />
+        {/* <Counter3 />
+        <Input /> */}
       </div>
     );
   }
