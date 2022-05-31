b0f81bc40016b9e3fee095eaf84f7600a249ec9e
delaying <pgy5638@naver.com>
2022-05-31 20:19:51 +0900

    공급자

---
+++ src/06/HomePageComponent.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+function RowBComponent() {
+  return <button>버튼</button>;
+}
+
+function HomePageComponent() {
+  return <div></div>;
+}
+
+export default HomePageComponent;
