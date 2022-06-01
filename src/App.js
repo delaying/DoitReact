8e93a6652b6f4c30eed89aa82aef7f805cac4dd5
delaying <pgy5638@naver.com>
2022-06-01 20:14:45 +0900

    컨텍스트

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import TodayPlan from './03/TodayPlan';
-import Counter3 from './03/Counter3';
-import Input from './03/Input';
+import HomePageComponent from './06/HomePageComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Counter3 />
-        <Input /> */}
+        <HomePageComponent />
       </div>
     );
   }
