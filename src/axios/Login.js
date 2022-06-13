221f6f4505799b7bff149f1f3808f8266f4b147c
delaying <pgy5638@naver.com>
2022-06-13 16:10:49 +0900

    로그인 연동 공부

---
+++ src/axios/Login.js
@@ -0,0 +1,12 @@
+import { render } from "enzyme";
+import React from "react";
+
+function Login(){
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return  <p>ㄹ로그인 페이지</p>;
+    }
+}
